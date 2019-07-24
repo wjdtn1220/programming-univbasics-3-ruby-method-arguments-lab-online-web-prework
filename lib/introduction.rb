b6d4ad4e7b3a04fb5779bbbff68462f155964031
Jeong Park <wjdtn1220@gmail.com>
2019-07-24 02:09:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,5 +8,8 @@ end
 
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  language = "Ruby"
+end
+
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
